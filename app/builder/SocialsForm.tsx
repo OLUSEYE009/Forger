60e7baf7daf5cf90d6bd48f776cda990466b6a78
Oyewole Oluseye <oyewoleseye009@gmail.com>
2025-12-10 13:36:29 +0100

    Update SocialsForm.tsx

--- app/builder/SocialsForm.tsx
+++ app/builder/SocialsForm.tsx
@@ -79,7 +79,7 @@ function SocialInput({
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <Icon className="h-5 w-5 text-gray-500" />
         </div>
-        <div className="pl-10" 
+        <div className="pl-10">
         <Input id={name} name={name} {...props} />
         </div>
       </div>
